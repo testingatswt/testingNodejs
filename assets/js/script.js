019516925e63123cc85cde02e1c516845b38ba56
yahyaizhar <yahyaizhar@gmail.com>
2018-05-07 20:18:37 +0500

    last commit

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,17 @@
 $(document).ready(function(){
 //  $('.myDiv').append('<input type="text" name="" value="Hello HII">');
+var jsonOBJ = [];
   $.get( "../assets/bin/whereamii.html", function( data ) {
+    $('<div>'+data+'</div>').find('a').each(function(){
+      jsonOBJ.push({
+        name: $(this).text()
+      });
+
+    });
+    //console.log(jsonOBJ);
       var data_compiled = $('<div>'+data+'</div>').find('a:first');
       var href = $('<div>'+data+'</div>').find('a').attr('href');
       $(data_compiled).attr('href', 'https://nomadlist.com/petracca'+href);
-      $( ".myDiv" ).html( data_compiled );
+      $( ".myDiv" ).html( JSON.stringify(jsonOBJ) );
     });
 });
