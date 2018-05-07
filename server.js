369e3fb0c48612b0076913210a74d658730ab561
yahyaizhar <yahyaizhar@gmail.com>
2018-05-07 20:57:25 +0500

    asdasd

--- server.js
+++ server.js
@@ -9,6 +9,11 @@ Object.assign=require('object-assign')
 app.engine('html', require('ejs').renderFile);
 app.use(morgan('combined'));
 app.use(express.static(__dirname + ''));
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 // var port = process.env.PORT || process.env.OPENSHIFT_NODEJS_PORT || 8080,
 //     ip   = process.env.IP   || process.env.OPENSHIFT_NODEJS_IP || '0.0.0.0';
